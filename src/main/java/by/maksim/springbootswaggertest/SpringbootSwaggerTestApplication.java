289f93352f7b21036d8fd5119e1356c90258cbfe
Maksim Yurau <2004softwaredeveloper@gmail.com>
2020-11-29 15:41:40 +0300

    Created new Spring Boot project. Updated pom.xml.

---
+++ src/main/java/by/maksim/springbootswaggertest/SpringbootSwaggerTestApplication.java
@@ -0,0 +1,13 @@
+package by.maksim.springbootswaggertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSwaggerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSwaggerTestApplication.class, args);
+    }
+
+}
