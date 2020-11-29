8a9335dc491f144746deb2c2aebf58f18320f9db
Maksim Yurau <2004softwaredeveloper@gmail.com>
2020-11-29 16:31:07 +0300

    Added ErrorResponse.java.

---
+++ src/main/java/by/maksim/springbootswaggertest/entity/ErrorResponse.java
@@ -0,0 +1,45 @@
+package by.maksim.springbootswaggertest.entity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.io.Serializable;
+
+@ApiModel(description = "Error Response Model")
+public class ErrorResponse implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+
+    @ApiModelProperty(notes = "Error Code", name = "code", value = "200")
+    private int id;
+
+    @ApiModelProperty(notes = "Status", name = "status", value = "SUCCESS")
+    private String status;
+
+    @ApiModelProperty(notes = "Message", name = "message", value = "Invalid field")
+    private String message;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
